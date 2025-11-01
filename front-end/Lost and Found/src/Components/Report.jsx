e189230a6a9259eb559f158645948d7ed73e5b98
Muhammad Furqan <muhammadfurqan3624515@gmail.com>
2025-11-01 12:01:51 +0500

    Update API URL to use environment variable

--- front-end/Lost and Found/src/Components/Report.jsx
+++ front-end/Lost and Found/src/Components/Report.jsx
@@ -36,7 +36,7 @@ function Report({ item, fetchLostItems, fetchFoundItems }) {
     };
 
     try {
-      const res = await fetch("http://localhost:8081/api/items", {
+      const res = await fetch(`${React_App_BACKENDURL}/api/items`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newItem),
